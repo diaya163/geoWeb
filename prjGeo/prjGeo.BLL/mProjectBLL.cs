c9a51a3e3c2e24e6fac6f9a73fc11507c445fa1f
hlhyd <hlhyd@163.com>
2018-03-06 20:19:49 +0800

    Project modify by 2018-03-06 by hulihai 1.在数据库中添加了[项目表] 2.更新了Model层的EF数据文件 3.在DAL中添加mProjectDAL类 4.在BLL中添加mProjectBLL类 5.在控制器中添加mProjectControll 6.根据控制器添加了视图文件index.chtml

---
+++ prjGeo/prjGeo.BLL/mProjectBLL.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Dynamic;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+using prjGeo.BLL.Core;
+using prjGeo.Models;
+using prjGeo.Models.Sys;
+using prjGeo.DAL;
+using prjGeo.Commons;
+namespace prjGeo.BLL
+{
+    /// <summary>
+    /// mProjectBLL
+    /// </summary>
+    public partial class mProjectBLL : BaseBLL
+    {
+        private readonly mProjectDAL dal = new mProjectDAL();
+        /// <summary>
+        /// 刷新数据
+        /// </summary>
+        /// <param name="filters">查询条件</param>
+        /// <param name="errMsg">错误信息</param>
+        /// <returns></returns>
+        public IList<mProject> GetList(string filters, ref string errMsg)
+        {
+            return dal.GetList(filters, db, ref errMsg);
+        }
+
+        /// <summary>
+        /// 新增数据
+        /// </summary>
+        /// <param name="model">实体类</param>
+        /// <param name="errMsg">错误信息</param>
+        /// <returns></returns>
+        public int Add(mProject model, ref string errMsg)
+        {
+            return dal.Add(model, db, ref errMsg);
+        }
+
+
+        /// <summary>
+        /// 修改数据
+        /// </summary>
+        /// <param name="model">实体类</param>
+        /// <param name="errMsg">错误信息</param>
+        /// <returns></returns>
+        public int Update(mProject model, ref string errMsg)
+        {
+            return dal.Update(model, db, ref errMsg);
+        }
+
+
+        /// <summary>
+        /// 删除数据
+        /// </summary>
+        /// <param name="model">实体类</param>
+        /// <param name="errMsg">错误信息</param>
+        /// <returns></returns>
+        public int Delete(mProject model, ref string errMsg)
+        {
+            return dal.Delete(model, db, ref errMsg);
+        }
+
+        public List<mProject> GetIndexList(string filters, ref string errMsg, ref GridPager pager)
+        {
+            var queryData = GetList(filters, ref errMsg);
+            var Queryable = queryData.AsQueryable();
+            var query = LinqHelper.DataSorting(Queryable, pager.sort, pager.order);
+            return CreateModelList(ref pager, ref query);
+        }
+        private List<mProject> CreateModelList(ref GridPager pager, ref IQueryable<mProject> queryData)
+        {
+            pager.totalRows = queryData.Count();
+            if (pager.totalRows > 0)
+            {
+                if (pager.page <= 1)
+                {
+                    queryData = queryData.Take(pager.rows);
+                }
+                else
+                {
+                    queryData = queryData.Skip((pager.page - 1) * pager.rows).Take(pager.rows);
+                }
+            }
+
+            var lstData = (from k in queryData
+                           select new mProject
+                           {
+                               id = k.id,
+                               Ccode = k.Ccode,
+                               Recid = k.Recid,
+                               CName = k.CName,
+                               RecName = k.RecName,
+                               Clocation = k.Clocation,
+                               Province = k.Province,
+                               City = k.City,
+                               County = k.County,
+                               Town = k.Town,
+                               Village = k.Village,
+                           }).ToList();
+            return lstData;
+
+        }
+    }
+}
+
+
---
+++ prjGeo/prjGeo.Web/Controllers/mProjectController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+using prjGeo.Commons;
+using prjGeo.BLL;
+using prjGeo.Models;
+using prjGeo.Models.Sys;
+
+namespace prjGeo.Web.Controllers
+{
+    public class mProjectController : BaseController
+    {
+
+        private string errMsg = string.Empty;
+        private mProjectBLL objBLL = new mProjectBLL();
+
+        public ActionResult Index()
+        {
+            ViewBag.Title = "项目维护";
+            //List<mUsersModel> list =objUser.GetAllList(),
+            var model = new
+            {
+                form = new
+                {
+
+                },
+                GridInfo = new
+                {
+                    idField = "id",
+                    ColInfo = new TableInfo().GetGridColInfo(114, 0),
+                    sortName = "id"
+                },
+                GridColInfo = new
+                {
+                    columns = new TableInfo().GetInitGridCols(114),
+                    rows = new TableInfo().GetInitGridRows(114)
+                }
+
+
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public JsonResult GetList(GridPager pager)
+        {
+            string filters = string.Empty;
+            var list = objBLL.GetIndexList(filters, ref errMsg, ref pager);
+            var json = new
+            {
+                total = pager.totalRows,
+                rows = list.ToArray()
+            };
+
+            return Json(json, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult SaveData(string action, mProject model)
+        {
+            if (action.Equals("new"))
+            {
+                objBLL.Add(model, ref errMsg);
+            }
+            else if (action.Equals("modify"))
+            {
+                objBLL.Update(model, ref errMsg);
+            }
+            return Json(new { errMsg }, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult Delete(mProject model)
+        {
+            objBLL.Delete(model, ref errMsg);
+            return Json(new { errMsg }, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult GetListData()
+        {
+            var list = objBLL.GetList(string.Empty, ref errMsg);
+            return Json(list, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
