d19e728faf0ce26a4f5a30b138bb2104d3d30763
hlhyd <hlhyd@163.com>
2018-03-06 21:28:58 +0800

    modify by 20180306 by hulihai 1.在Model中添加了分部类mProject

---
+++ prjGeo/prjGeo.Models/mProject.ljx.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prjGeo.Models
+{
+    public partial class mProject
+    {
+        public string RecName { get; set; }
+    }
+}
